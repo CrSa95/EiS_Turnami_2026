2efb4a20d35eafca9ffa94aa94c47087a8f36811
PullolilEzequiel <ezequielkarottupullolil@gmail.com>
2026-09-06 19:01:41 -0300

    Se fixeo error de buildeo - Se agrego guia para levantar la app

--- api/src/dao/medico.dao.ts
+++ api/src/dao/medico.dao.ts
@@ -1,4 +1,4 @@
-import MedicoModel, {IMedico } from '../model/medico.model.js';
+import MedicoModel, {IMedico } from '../model/medico.model';
 export default class MedicoDAO {
     async findByDNI(dni: string): Promise<IMedico | null> {
         return await MedicoModel.findOne({ dni }).exec();
--- api/src/dao/paciente.dao.ts
+++ api/src/dao/paciente.dao.ts
@@ -1,4 +1,4 @@
-import PacienteModel, {IPaciente } from '../model/paciente.model.js';
+import PacienteModel, {IPaciente } from '../model/paciente.model';
 export default class PacienteDAO {
     async findByDNI(dni: string): Promise<IPaciente | null> {
         return await PacienteModel.findOne({ dni }).exec();
