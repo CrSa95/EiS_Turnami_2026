06110831729c4ffe68f328eadcafdda14736ddd4
Matias Dominguez <dmzmatias32@gmail.com>
2026-09-07 17:36:39 -0300

    Creación inicial de user medico

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import PacienteModel from './model/paciente.model.js';
+import MedicoModel from './model/medico.model.js';
 import PasswordService from './services/password.services.js';
 dotenv.config();
 
@@ -9,6 +10,21 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "http://localhost"
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://root:secretpassword@127.0.0.1:27017/turnami_db?authSource=admin';
+
+async function seedMedicoDePrueba() {
+  const dni = '11223344';
+  const medicoExistente = await MedicoModel.findOne({ dni }).exec();
+
+  if (!medicoExistente) {
+    await MedicoModel.create({
+      dni,
+      password: await PasswordService.hash('1234'),
+      nombre: 'Juan',
+      apellido: 'Perez',
+    });
+    console.log(' Medico de prueba creado');
+  }
+}
 
 async function seedPacienteDePrueba() {
   const dni = '00000000';
@@ -29,6 +45,7 @@ async function bootstrap() {
   try {
     await mongoose.connect(MONGO_URI);
     console.log(' Conectado a MongoDB');
+    await seedMedicoDePrueba();
     await seedPacienteDePrueba();
 
     app.listen(PORT, () => {
