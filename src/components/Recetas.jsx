fb3e89f4a831aeff5aaa411547f018f9131aea82
NarelaCamara <roxana.camara07@gmail.com>
2026-09-08 21:13:01 -0300

    feat: componentes basicos

---
+++ src/components/Recetas.jsx
@@ -0,0 +1,36 @@
+function Recetas({ rol, recetas }) {
+
+    const rowReceta = (e) => {
+        return <div> <div>
+            {e.receta.nombre}</div>  <div>
+                {e.paciente.nombre}</div> <div>
+                {e.receta.descripcion}</div> </div>
+
+    }
+
+    const headersRecetas = () => {
+        return <div>
+            <div>
+            Nombre Paciente
+        </div>
+        <div>
+            Medicamento
+        </div>
+        </div>
+    }
+
+    return <div>
+        <h1>Recetas</h1>
+        {
+            headersRecetas()
+        }
+        {
+            recetas.map(e => {
+                return rowReceta(e)
+            })
+        }
+    </div>
+
+}
+
+export default Recetas;
