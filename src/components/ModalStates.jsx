96f7367d22a72e3a80bcc1b23ad5f1da71eb488e
NarelaCamara <roxana.camara07@gmail.com>
2026-09-10 00:55:53 -0300

    feat: 3 segundos

--- src/components/ModalStates.jsx
+++ src/components/ModalStates.jsx
@@ -9,7 +9,7 @@ function ModalState({ state, setState }) {
     useEffect(() => {
         if (state !== States.Ok && state !== States.Error) return
 
-        const timeoutId = setTimeout(() => setState('NONE'), 5000)
+        const timeoutId = setTimeout(() => setState('NONE'), 3000)
         return () => clearTimeout(timeoutId)
     }, [state, setState])
 
